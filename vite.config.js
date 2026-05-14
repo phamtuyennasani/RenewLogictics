fcb1c3bbdc0926ca5107917730f1df194ae37874
Pham Tuyen <phamtuyen.nasani@gmail.com>
2026-05-14 23:56:28 +0700

    Add Livewire manifest file for asset management

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            publicDirectory: 'public_html',
             refresh: [
                 'resources/views/**',
                 'routes/**',
